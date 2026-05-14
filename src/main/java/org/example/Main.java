debcaa7e3e85f60b76136b32da9918ce12887c8c
DeivisRM <djemdliam@gmail.com>
2026-05-13 23:28:09 -0500

    estructurando CuentaAhorroRepository y Cuenta

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.model.CuentaAhorro;
 import org.example.repository.ClienteRepository;
+import org.example.repository.CuentaAhorroRepository;
 
 public class Main {
     public static void main() {
@@ -12,5 +14,24 @@ public class Main {
         // Luego procedemos con la lógica
         repository.insertCliente();
         repository.selectCliente();
+    }
+    // Ejemplo de uso en el Main
+    public static void main(String[] args) {
+        CuentaAhorroRepository repo = new CuentaAhorroRepository();
+        // Ejecutando primero createTable
+        repo.createTable();
+        // 1. Supongamos que traes la cuenta de la DB con ID 1 y saldo 1500.50
+        CuentaAhorro miCuenta = new CuentaAhorro(1, 1, "191-74291919-0", "AHORRO", 1500.50);
+
+        try {
+            // 2. Realizas la operación en el objeto Java (Memoria)
+            miCuenta.retirar(500.0);
+
+            // 3. Guardas el cambio en PostgreSQL (Persistencia)
+            repo.updateSaldo(miCuenta.getId(), miCuenta.getSaldo_actual());
+
+        } catch (IllegalArgumentException e) {
+            System.err.println("Operación fallida: " + e.getMessage());
+        }
     }
 }
--- src/main/java/org/example/model/CuentaCorriente.java
+++ src/main/java/org/example/model/CuentaCorriente.java
@@ -1,19 +1,19 @@
 package org.example.model;
 
 public class CuentaCorriente extends Cuenta {
-    public CuentaCorriente(String id, String titular, Double saldo) {
-        super(id, titular, saldo);
+    public CuentaCorriente(int id, int Cliente_id,String numero_cuenta, String tipo_cuenta, Double saldo_actual) {
+        super(id, Cliente_id, numero_cuenta, tipo_cuenta, saldo_actual);
     }
 
     @Override
     public void depositar(Double monto) {
-        System.out.println("Depositando " + monto + " en la cuenta corriente de " + getTitular());
+        System.out.println("Depositando " + monto + " en la cuenta corriente de " + getCliente_id());
     }
 
     @Override
     public void retirar(Double monto){
-        System.out.println("Retirando " + monto + " de la cuenta corriente de " + getTitular());
-        if (getSaldo() + 500 >= monto) {
+        System.out.println("Retirando " + monto + " de la cuenta corriente de " + getCliente_id());
+        if (getSaldo_actual() + 500 >= monto) {
             System.out.println("El sobregiro permitido");
         } else {
             throw new IllegalArgumentException("No se puede retirar más de lo que hay en la cuenta" + monto);
